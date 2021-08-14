564a99600afd1619662cb156100536c937ead2b8
Afeef <86735357+MM2271@users.noreply.github.com>
2021-08-14 16:56:13 +0300

    react intro with sample landing page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from './Button.js';
+import Card from './Card.js';
+import NavBar from './NavBar';
+import Hero from './Hero'
+
+const travelLocations = [
+  {
+    image: "https://images.unsplash.com/photo-1628887391400-0c8cc8f3fac1?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    title: "Berchtesgaden, Germany",
+    description: "Lovely travel destination",
+    buttonLabel: "Visit",
+    buttonLink: "https://..."
+  },
+  {
+    image: "https://images.unsplash.com/photo-1476362174823-3a23f4aa6d76?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+    title: "Florence, Italy",
+    description: "Lovely travel destination",
+    buttonLabel: "Visit",
+    buttonLink: "https://..."
+  },
+  {
+    image: "https://images.unsplash.com/photo-1623674587543-9c7564de99d1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1949&q=80",
+    title: "Giza, Egypt",
+    description: "Lovely travel destination",
+    buttonLabel: "Visit",
+    buttonLink: "https://..."
+  },
+  {
+    image: "https://images.unsplash.com/photo-1548786811-dd6e453ccca7?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cGV0cmF8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    title: "Petra, Jordan",
+    description: "Lovely travel destination",
+    buttonLabel: "Visit",
+    buttonLink: "https://..."
+  }
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <NavBar logo="BTA"/>
+      <Hero 
+        title="Bootstrap Travel Agency"
+        description="Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins."
+        buttonLabel1="Book Now"
+        buttonLabel2="Learn More"
+      />
+
+      <div className="container">
+        <div className="row">
+            {
+              travelLocations.map(
+                (location) => {
+                  return (
+                    <div className="col-12 col-md-3 mb-3">
+                      <Card 
+                        image={location.image}
+                        title={location.title}
+                        description={location.description}
+                        buttonLabel={location.buttonLabel}
+                        buttonLink={location.buttonLink}
+                      />
+                    </div>
+                  )
+                }
+              )
+            }
+        </div>
+      </div>
     </div>
   );
 }
 
+
+/*function App() {
+  return (
+    <div>
+      <Button link="/home" width="100px">Home</Button> 
+      <Button link="/about" width="100px">About</Button>
+      <Button link="/contact" width="300px">Register</Button>
+
+      <Card image="https://images.unsplash.com/photo-1628847022112-822475a94a78?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" 
+      title="Card101" description="Card description random" link="/home">CardButton101</Card>
+      <Card image="https://images.unsplash.com/photo-1628711632211-50b28c0cce4a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1085&q=80" 
+      title="Some Animal" description="a deer i guess" link="/animal">CardButton102</Card>
+      <Card image="https://images.unsplash.com/photo-1628797279405-8cd6ffdbeb6c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" 
+      title="Room" description="A kitchen" link="/kitchen">CardButton103</Card>
+    </div>
+  );
+}
+
+
+
+/*function MyButton(props) {
+  return (
+    <button className={"btn " + props.color}>{ props.children }</button>
+  )
+}
+
+function Button(props) {
+  return (
+    <button className={"btn" + props.width}>{ props.children }</button>
+  )
+}
+
+function Button1(props) {
+  return (
+    <button style={ { width: props.width } } className="btn btn-primary">{props.children}</button>
+  )
+}
+
+function App() {
+  return (
+    <div>
+       <Button width=" btn-primary btn-lg">Large</Button>
+       <Button width=" btn-secondary btn-sm">Small</Button>
+       <Button width="btn-primary">Normal</Button>
+      <br></br>
+       <Button1 width="100px">Large</Button1>
+       <Button1 width=" 150px">Small</Button1>
+       <Button1 width="200px">Normal</Button1>
+    </div>
+  );
+}*/
+
 export default App;
+
